d527f793266bc03fb5f5b410fb3ba0a198ec6453
Vishnu Nair <visair1504@gmail.com>
2022-11-19 21:18:50 +0530

    Add all my files

---
+++ index.js
@@ -0,0 +1,25 @@
+const puppeteer=require('puppeteer');
+
+(async()=>{
+    const browser=await puppeteer.launch();
+    const page=await browser.newPage();
+    await page.goto("https://www.reddit.com/r/learnprogramming/")
+    const search= await page.evaluate(()=>{
+        const search_results=document.querySelectorAll("._1oQyIsiPHYt6nx7VOmd1sz._1RYN-7H8gYctjOQeL8p2Q7.scrollerItem._3Qkp11fjcAw9I9wtLo8frE._1qftyZQ2bhqP62lbPjoGAh");
+        const lists=[];
+        search_results.forEach(elements => {
+            const votes=elements.querySelector("._23h0-EcaBUorIHC-JZyh6J  ._1E9mcoVn4MYnuBQSVDt1gC ._1rZYMD_4xY3gRcSS3p8ODO._3a2ZHWaih05DgAOtvu6cIo");
+            const question=elements.querySelector("h3");
+
+            lists.push({
+                vote_count:votes.innerText
+                ,question_main:question.innerText
+            });
+        });
+        return lists;
+
+    })
+    console.log(search);
+    await browser.close();
+})()
+console.log("END");
