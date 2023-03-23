589692030de451d75d3e3f4c9d2f777c16072f1b
Vishnu Nair <62656054+Vish1504@users.noreply.github.com>
2023-03-23 07:16:29 +0530

    made it a custom csv file

--- index.js
+++ index.js
@@ -1,37 +1,61 @@
-const puppeteer=require('puppeteer');
-const json2csv=require("json2csv").Parser;
-const fs=require("fs");
+const puppeteer = require('puppeteer');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const subreddit = 'node';
+const scrapeReddit = async () => {
 
-(async()=>{
-    const browser=await puppeteer.launch();
-    const page=await browser.newPage();
-    await page.goto("https://www.reddit.com/r/node/")
-    const search= await page.evaluate(()=>{
-        const search_results=document.querySelectorAll("._1oQyIsiPHYt6nx7VOmd1sz._1RYN-7H8gYctjOQeL8p2Q7.scrollerItem._3Qkp11fjcAw9I9wtLo8frE._1qftyZQ2bhqP62lbPjoGAh");
-        const lists=[];
-        search_results.forEach(elements => {
-            const votes=elements.querySelector("._23h0-EcaBUorIHC-JZyh6J  ._1E9mcoVn4MYnuBQSVDt1gC ._1rZYMD_4xY3gRcSS3p8ODO._3a2ZHWaih05DgAOtvu6cIo");
-            const comments=elements.querySelector(".FHCV02u6Cp2zYL0fhQPsO");
-            const question=elements.querySelector("h3");
-            if(votes.innerText=='Vote')
-                {
-                    votes.innerText='0'
-                }
-            lists.push({
-                vote_count:votes.innerText,
-                comments_count:parseInt(comments.innerText.replace(/[^0-9\.]/g, ''), 10)
-                ,question_main:question.innerText
-            });
-        });
-        return lists;
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://www.reddit.com/r/${subreddit}/`);
 
-    })
-    // console.log(search);
-    
+    let results = [];
+    let count = 0;
+
+    while (results.length < 100) {
+        const elements = await page.$$('._1oQyIsiPHYt6nx7VOmd1sz._1RYN-7H8gYctjOQeL8p2Q7.scrollerItem._3Qkp11fjcAw9I9wtLo8frE._1qftyZQ2bhqP62lbPjoGAh');
+        for (const element of elements) {
+            const title = await element.$eval('h3', (node) => node.innerText.trim());
+            if (title) {
+                let score = await element.$eval('._1rZYMD_4xY3gRcSS3p8ODO', (node) => node.innerText.trim());
+                score = score === 'Vote' ? 0 : score;
+                const comments = await element.$eval('.FHCV02u6Cp2zYL0fhQPsO', (node) => node.innerText.trim());
+                results.push({
+                    title,
+                    score,
+                    comments,
+                });
+            }
+            count++;
+            if (results.length >= 100 || count >= 100) {
+                break;
+            }
+        }
+        if (results.length >= 100 || count >= 100) {
+            break;
+        }
+        const lastElement = elements[elements.length - 1];
+        await page.evaluate((el) => el.scrollIntoView(), lastElement);
+        await page.waitForTimeout(1000);
+    }
+
     await browser.close();
-    const j2cp=new json2csv();
-    const csv=j2cp.parse(search);
+    return results;
+};
 
-    fs.writeFileSync("./reddit.csv",csv,"utf-8");
-})()
-console.log("END");
+const saveToCsv = async (data) => {
+    const csvWriter = createCsvWriter({
+        path: `${subreddit}.csv`,
+        header: [
+            { id: 'title', title: 'Title' },
+            { id: 'score', title: 'Score' },
+            { id: 'comments', title: 'Comments' },
+        ],
+    });
+
+    await csvWriter.writeRecords(data);
+};
+
+(async () => {
+    const results = await scrapeReddit();
+    await saveToCsv(results);
+    console.log('Data scraped and saved to csv!');
+})();
